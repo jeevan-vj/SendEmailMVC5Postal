6d27fb1432e899a5f850df1ef743fac030128e08
jeevan wijerathna <jeevan90wijerathna@outlook.com>
2017-03-10 13:02:50 +0530

    Initial

---
+++ SendEmailMVC5/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Postal;   
+using System.Web.Mvc;
+
+namespace SendEmailMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SendMail()
+        {
+            dynamic email = new Email("Example");
+            email.To = "jeevan90wijerathna@gmail.com";
+            email.Message = "test by jeevan";
+            email.Send();
+            return View();
+        }
+    }
+}
